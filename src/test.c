e00afb58fd39ab154a86ed170293e5af884069a9
JM7603 <jm7603@126.com>
2023-02-23 18:14:43 +0800

    comments

--- src/test.c
+++ src/test.c
@@ -5,27 +5,24 @@
 
 #pragma comment(lib, "libcurl.a")
 
-// gcc .\test.c -o ..\out\test.exe 
+// gcc .\test.c -o ..\out\test.exe
 // -DNDEBUG -static -L ..\lib -lcurl -lws2_32 -lwinmm -lwldap32 -lbcrypt
- 
+
 int main(void) {
   CURL *curl;
   CURLcode res;
- 
+
   curl = curl_easy_init();
-  if(curl) {
+  if (curl) {
     curl_easy_setopt(curl, CURLOPT_URL, "http://www.baidu.com");
-    /* example.com is redirected, so we tell libcurl to follow redirection */
+
     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
- 
-    /* Perform the request, res will get the return code */
+
     res = curl_easy_perform(curl);
-    /* Check for errors */
-    if(res != CURLE_OK)
+    if (res != CURLE_OK)
       fprintf(stderr, "curl_easy_perform() failed: %s\n",
               curl_easy_strerror(res));
- 
-    /* always cleanup */
+
     curl_easy_cleanup(curl);
   }
   return 0;
